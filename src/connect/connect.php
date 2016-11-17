ffa7fbfc3b37b85e97c0368d6993846305ddd005
Farzana0627 <sinceramuna@gmail.com>
2016-11-17 19:06:34 +0600

    Add files via upload

---
+++ src/connect/connect.php
@@ -0,0 +1,9 @@
+<?php
+	$host = "127.0.0.1";
+	$username = "root";
+	$password = "";
+	$db_name = "cms";
+
+	$connection = mysqli_connect($host, $username, $password) or die("Host not connected");
+	mysqli_select_db($connection, $db_name) or die("DB not selected");
+?>
