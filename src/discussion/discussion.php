cd94f4ddf885386d4356effd02ea4fa613be95f6
numan7852 <bsse0624@iit.du.ac.bd>
2016-11-17 19:26:04 +0600

    Add files via upload

--- src/discussion/discussion.php
+++ src/discussion/discussion.php
@@ -2,154 +2,224 @@
 <html lang="en">
 
 <head>
+
     <!-- Required meta tags always come first -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>CMS | IITDU | Course Discussion</title>
+    <title>CMS | IITDU</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
 
+    <!-- CSS -->
+
     <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Material Design Bootstrap -->
-    <link href="css/mdb.min.css" rel="stylesheet">
+    <link href="../css/mdb.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
 
 </head>
 
-<body>
+<body class="fixed-sn blue-skin index-body">
 
-	<div id="discussion" class="container-fluid">
-		<div class="card card-block" align="center">
-			<h3 class="card-title">Course Name: Course A</h3>
-		</div>
-		<div class="row">
-			<div class="col-sm-8">
-				<div class="card">
-					<!--Accordion wrapper-->
-					<div class="accordion" id="accordion" role="tablist" aria-multiselectable="true">
+    <?php
 
-					    <!--First Question-->
-					    <div class="panel panel-default">
+        session_start();
 
-					        <!--Panel heading-->
-					        <div class="panel-heading" role="tab" id="questionOne">
-					            <h5 class="panel-title">
-									<a class="arrow-r" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Q1. What is an Operating System?<i class="fa fa-angle-down rotate-icon"></i>
-									</a>
-								</h5>
-					        </div>
+        if (!isset($_SESSION['login_id'])) {
+            header ("Location: http://localhost:8080/cms/login.php");
+        }
 
-					        <!--Panel body-->
-					        <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="questionOne">
+        elseif (isset($_GET['course_code'])) {
+            
+            $course_code = $_GET['course_code'];
+        }
 
-					        	<!-- Answer-1 -->
-					            <div>
-					            	<h5 style="font-weight: bold;">Anik: </h5>
-					            	<p class="text-justify">
-					            		An operating system (OS) is system software that manages computer hardware and software resources and provides common services for computer programs. All computer programs, excluding firmware, require an operating system to function. Time-sharing operating systems schedule tasks for efficient use of the system and may also include accounting software for cost allocation of processor.
-					            	</p>
-					            </div>
+        else {
 
-					            <!-- Answer-2 -->
-					            <div>
-					            	<h5 style="font-weight: bold;">Tawsif: </h5>
-					            	<p class="text-justify">
-					            		Its nothing but normal Os that can operate a machine in a very efficeint way that can handle the risk.
-					            	</p>
-					            </div>
-					            <hr>
-					            <div class="md-form">
-					            	<textarea type="text" id="answer_for_q1" class="md-textarea"></textarea>
-					            	<label for="answer_for_q1">write an answer</label>
-					            </div>
+            header ("Location: http://localhost:8080/cms/");
+        }
 
-					             <div>
-					                <button type="submit" class="btn btn-purple btn-md">Submit answer</button>
-					             </div>
-					        </div>
-					    </div>
-					    <!--/.First question-->
 
-					    <!--Second Question-->
-					    <div class="panel panel-default">
+    ?>
 
-					        <!--Panel heading-->
-					        <div class="panel-heading" role="tab" id="questionTwo">
-					            <h5 class="panel-title">
-									<a class="arrow-r" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">Q2. What is an Normal OS?<i class="fa fa-angle-down rotate-icon"></i>
-									</a>
-								</h5>
-					        </div>
+    <?php
 
-					        <!--Panel body-->
-					        <div id="collapseTwo" class="panel-collapse collapse" role="tabpane1" aria-labelledby="questionTwo">
-					            <h5 style="font-weight: bold;">Jamil: </h5>
-					            <p class="text-justify">
-					            	An operating system (OS) is system software that manages computer hardware and software resources and provides common services for computer programs. All computer programs, excluding firmware.
-					            </p>
-					            <hr>
-					            <div class="md-form">
-					            	<textarea type="text" id="answer_for_q2" class="md-textarea"></textarea>
-					            	<label for="answer_for_q2">write an answer</label>
-					            </div>
+        include '../connect/connect.php';
+        include '../header/header.php';
 
-					             <div>
-					                <button type="submit" class="btn btn-purple btn-md">Submit answer</button>
-					             </div>
-					        </div>
-					    </div>
-					    <!--/.Second question-->
-					</div>
-					<!--/.Accordion wrapper-->
-				</div>
-			</div>
+    ?>
 
-			<div class="col-sm-4">
-				<div class="card">
-				    <div class="card-block">
 
-				        <!--Header-->
-				        <div>
-				        	<h3>Ask question</h3>
-				        </div>
+    <div id="discussion_list" class="container-fluid">
 
-				        <!--Body-->
+        <div class="card card-block" align="center">
 
-				      	<div class="md-form">
-				      	    <textarea type="text" id="question" class="md-textarea"></textarea>
-				      	    <label for="question">Question </label>
-				      	</div>
+        <?php
 
-				        <div class="text-xs-center">
-				            <button class="btn btn-success btn-block"><i class="fa fa-plus" aria-hidden="true"></i></button>
-				        </div>
+            $sql = "SELECT course_name FROM course WHERE course_code='$course_code'";
+            $result = mysqli_query($connection,$sql);
+            $row = mysqli_fetch_assoc($result);
 
-				    </div>
-				</div>
-			</div>
-		</div>
-	</div>
+            echo '<h3 class="card-title">Course : '.$row['course_name'].'</h3>';
+
+        ?>
+            
+        </div>
+
+        <div class="row">
+            <div class="col-sm-8">
+
+                <span id="question_answer"></span>
+                
+            </div>
+
+            <div class="col-sm-4">
+                <div class="card">
+                    <div class="card-block">
+
+                        <!--Header-->
+                        <div>
+                            <h3>Ask Question</h3>
+                        </div>
+
+                        <!--Body-->
+
+                        <div class="md-form">
+                            <textarea type="text" id="question_body" class="md-textarea"></textarea>
+                            <label for="question_body">Question </label>
+                        </div>
+
+                        <div class="text-xs-center">
+                            <button class="btn btn-success btn-block" onclick="ask_question()"><i class="fa fa-plus" aria-hidden="true"></i></button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <!--Footer-->
+    <footer class="my-footer container-fluid">
+
+        <!--Copyright-->
+            <div class="flex-center">
+                © 2016 Copyright - IITDU
+            </div>
+        <!--/.Copyright-->
+
+    </footer>
+    <!--/.Footer-->
+
 
     <!-- SCRIPTS -->
 
     <!-- JQuery -->
-    <script type="text/javascript" src="js/jquery-2.2.3.min.js"></script>
+    <script type="text/javascript" src="../js/jquery-2.2.3.min.js"></script>
 
     <!-- Tooltips -->
-    <script type="text/javascript" src="js/tether.min.js"></script>
+    <script type="text/javascript" src="../js/tether.min.js"></script>
 
     <!-- Bootstrap core JavaScript -->
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
 
     <!-- MDB core JavaScript -->
-    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <script type="text/javascript" src="../js/mdb.min.js"></script>
 
     <!-- Jquery Preloader Plugin -->
-    <script type="text/javascript" src="js/jquery.preloader.min.js"></script>
+    <script type="text/javascript" src="../js/jquery.preloader.min.js"></script>
+
+    <!-- Header and Preloader Js -->
+    <script type="text/javascript" src="../js/header.js"></script>
+
+    <script type="text/javascript">
+
+    $(document).ready(function(){
+        question_answer();
+    });
+        
+    function ask_question(){
+
+        var question_body = $('#question_body').val();
+        var course_code = '<?php echo $course_code;?>';
+        var name = '<?php
+
+                        $id = $_SESSION['login_id'];
+                        
+                        if ($type=="student"){
+                            $sql = "SELECT name FROM student WHERE student_id='$id'";    
+                                                                            }
+                        else {
+                            $sql = "SELECT name FROM user WHERE user_id='$id'";
+                        }
+                                                                            
+                        $result = mysqli_query($connection, $sql);
+                        $row = mysqli_fetch_assoc($result);
+
+                        echo $row['name'];
+
+                    ?>';
+
+/*        console.log(question_body);
+        console.log(course_code);
+        console.log(name);*/
+
+        $.post("helper/ask_question.php",{question_body:question_body, course_code:course_code, name:name}, function(result){
+            toastr.info(result);
+            $('#question_body').val("");
+            question_answer();
+        });
+    }
+
+    function question_answer(){
+
+        var course_code = '<?php echo $course_code;?>';
+
+        $.post("helper/show_question.php", {course_code:course_code}, function(result){
+            $('#question_answer').html(result);    
+        });
+        
+    }
+
+    function save_answer(question_id){
+
+        var answer = $('#answer-'+question_id).val();
+        var name = '<?php
+
+                                $id = $_SESSION['login_id'];
+                                
+                                if ($type=="student"){
+                                    $sql = "SELECT name FROM student WHERE student_id='$id'";    
+                                                                                    }
+                                else {
+                                    $sql = "SELECT name FROM user WHERE user_id='$id'";
+                                }
+                                                                                    
+                                $result = mysqli_query($connection, $sql);
+                                $row = mysqli_fetch_assoc($result);
+
+                                echo $row['name'];
+
+                            ?>';
+
+        /*alert(question_id);
+        alert(answer);
+        alert(name);*/
+
+        $.post("helper/save_answer.php",{question_id:question_id, answer:answer, name:name}, function(result){
+            toastr.info(result);
+            question_answer();
+        });
+    }
+
+    </script>
+
 
 </body>
 
