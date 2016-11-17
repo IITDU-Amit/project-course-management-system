d8826d019396db3b89558e21f0154614ace7ed3f
numan7852 <bsse0624@iit.du.ac.bd>
2016-11-17 19:25:31 +0600

    Add files via upload

---
+++ src/template.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>CMS | IITDU</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+
+    <!-- CSS -->
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Material Design Bootstrap -->
+    <link href="../css/mdb.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+
+</head>
+
+<body class="fixed-sn blue-skin index-body">
+
+    <?php
+
+        session_start();
+
+        if (!isset($_SESSION['login_id'])) {
+            header ("Location: http://localhost:8080/cms/login.php");
+        }
+
+        elseif ($_SESSION['login_type']!="committee") {
+            header ("Location: http://localhost:8080/cms/");
+        }
+    ?>
+
+    <?php
+
+        include '../connect/connect.php';
+        include '../header/header.php';
+
+    ?>
+
+
+
+    <!--Footer-->
+    <footer class="my-footer container-fluid">
+
+        <!--Copyright-->
+            <div class="flex-center">
+                © 2016 Copyright - IITDU
+            </div>
+        <!--/.Copyright-->
+
+    </footer>
+    <!--/.Footer-->
+
+
+    <!-- SCRIPTS -->
+
+    <!-- JQuery -->
+    <script type="text/javascript" src="../js/jquery-2.2.3.min.js"></script>
+
+    <!-- Tooltips -->
+    <script type="text/javascript" src="../js/tether.min.js"></script>
+
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="../js/mdb.min.js"></script>
+
+    <!-- Jquery Preloader Plugin -->
+    <script type="text/javascript" src="../js/jquery.preloader.min.js"></script>
+
+    <!-- Header and Preloader Js -->
+    <script type="text/javascript" src="../js/header.js"></script>
+
+
+</body>
+
+</html>
